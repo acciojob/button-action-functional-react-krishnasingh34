1b33f0543e5be95f7b33cc7c4e0bdced29ab350e
Krishna <singhkrishna9689@gmail.com>
2025-03-31 15:43:25 +0530

    submission

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,14 @@
-import React, {Component, useState} from "react";
+import React, { Component, useState } from "react";
 import "./../styles/App.css";
 
 function App() {
- const [para, setPara] = useState('')
+  const [para, setPara] = useState('')
   return (
     <div id="main">
-      <button id='click' onClick={()=> {
+      <button id='click' onClick={() => {
         setPara("Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy")
       }}>Submit</button>
-      <p id='para'>{para}</p>
+      {para && <p id="para">{para}</p>}
 
     </div>
   );
