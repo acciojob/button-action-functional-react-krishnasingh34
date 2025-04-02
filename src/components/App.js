049b17edbee3e3e9b3acfd716b1b13774367e676
Krishna <singhkrishna9689@gmail.com>
2025-04-02 23:44:33 +0530

    submission

--- src/components/App.js
+++ src/components/App.js
@@ -1,17 +1,35 @@
+// import React, {Component, useState} from "react";
+// import "./../styles/App.css";
+
+// function App() {
+//  const [para, setPara] = useState('')
+//   return (
+//     <div id="main">
+//       <button id='click' onClick={()=> {
+//         setPara("Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy")
+//       }}>Submit</button>
+//       {para && <p id='para'>{para}</p>} 
+//     </div>
+//   );
+// }
+
+// export default App;
+
 import React, {Component, useState} from "react";
 import "./../styles/App.css";
 
 function App() {
- const [para, setPara] = useState('')
+ const [click, setClick] = useState(false)
   return (
     <div id="main">
       <button id='click' onClick={()=> {
-        setPara("Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy")
+        setClick(true)
       }}>Submit</button>
-      {para && <p id='para'>{para}</p>} 
+      {
+      click==true && <p id='para'>Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy</p>
+      } 
     </div>
   );
 }
-
 
 export default App;
